9e0744f901cbbf5c782e89a4f19e7761788bbd87
Andreas Mårtensson <addem1234@gmail.com>
2016-02-23 15:10:40 +0100

    Started work on frontend

--- server.js
+++ server.js
@@ -2,22 +2,58 @@ var express    = require('express');
 var bodyParser = require('body-parser');
 var pg         = require('pg');
 var squel      = require('squel');
+var nunjucks   = require('nunjucks');
+var fs         = require('fs');
+
+var config     = require('./config');
 
 var app        = express();
-var PORT       = process.env.PORT || 8000;
-var conString  = process.env.POSTGRES_URL;
-console.log(conString);
-var client = new pg.Client(conString);
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+
+var client = new pg.Client(config.conString);
 client.connect(function(err) {
   if(err) console.log('Failed to connect to database', err);
   else    console.log('Database connection successful');
 })
 
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.get('/', function(req, res) {
+  var query = squel
+    .select('*')
+      .from('course')
+    .toString();
+
+  client.query(query, function(err, result) {
+    if(err) res.send(err);
+    else    res.render('index.html', result);
+    console.log(result.rows);
+  });
+});
+
+app.post('/recitation', function(req, res) {
+  console.log(req.body);
+  var query = squel
+    .select('*')
+      .from('recitations')
+      .where('cid=' + req.body.cid)
+    .toString()
+
+  client.query(query, function(err, result) {
+    if(err) res.send(err);
+    else    res.send(result);
+    console.log(result);
+  })
+});
+
 app.post('/solve', function(req, res) {
-  var body = res.body;
+  var body = req.body;
 
   var query = squel
     .insert()
@@ -27,7 +63,6 @@ app.post('/solve', function(req, res) {
       .set('called', false)
       .set('track',  body.track)
       .set('u1',     body.u1)
-      .set('u2',     body.u2)
       .set('u2',     body.u2)
       .set('u3',     body.u3)
     .toString()
@@ -52,9 +87,5 @@ app.post('/call', function(req, res) {
   });
 });
 
-app.get('/', function(req, res) {
-
-});
-
-app.listen(PORT);
-console.log('Listening on port', PORT);
+app.listen(config.PORT);
+console.log('Listening on port', config.PORT);
